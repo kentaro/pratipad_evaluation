cc1d61989ddd3a7eae0cd7554a35d81ed52691f5
Kentaro Kuribayashi <kentarok@gmail.com>
2022-05-05 21:59:37 +0900

    make public

--- server/lib/pratipad/example/server/application.ex
+++ server/lib/pratipad/example/server/application.ex
@@ -6,7 +6,7 @@ defmodule Pratipad.Example.Server.Application do
   @impl true
   def start(_type, _args) do
     Logger.configure(level: :info)
-    :ets.new(:counter, [:set, :protected, :named_table])
+    :ets.new(:counter, [:set, :public, :named_table])
     :ets.insert_new(:counter, {:count, 0})
 
     children = [
